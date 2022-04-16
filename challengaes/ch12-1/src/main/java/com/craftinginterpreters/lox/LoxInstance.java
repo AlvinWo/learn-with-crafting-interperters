3f4b2dda91fb9e24ea7b9bd255a0d3729411d921
Alvin Wo <alvin.wo@airbnb.com>
2022-04-16 14:18:28 +0800

    finish challenge 12.1

--- challengaes/ch12-1/src/main/java/com/craftinginterpreters/lox/LoxInstance.java
+++ challengaes/ch12-1/src/main/java/com/craftinginterpreters/lox/LoxInstance.java
@@ -5,29 +5,29 @@ import java.util.Map;
 
 public class LoxInstance {
 
-    private final LoxClass klass;
-    private final Map<String, Object> fields = new HashMap<>();
+  private final LoxClass klass;
+  private final Map<String, Object> fields = new HashMap<>();
 
-    public LoxInstance(LoxClass loxClass) {
-        this.klass = loxClass;
+  public LoxInstance(LoxClass loxClass) {
+    this.klass = loxClass;
+  }
+
+  public Object get(Token name) {
+    if (fields.containsKey(name.lexeme)) {
+      return fields.get(name.lexeme);
     }
 
-    public Object get(Token name) {
-        if (fields.containsKey(name.lexeme)) {
-            return fields.get(name.lexeme);
-        }
+    LoxFunction method = klass.findMethod(name.lexeme);
+    if (method != null) return method.bind(this);
+    throw new RuntimeError(name, "Undefined property '" + name.lexeme + "'.");
+  }
 
-        LoxFunction method = klass.findMethod(name.lexeme);
-        if (method != null) return method.bind(this);
-        throw new RuntimeError(name, "Undefined property '" + name.lexeme + "'.");
-    }
+  public void set(Token name, Object value) {
+    fields.put(name.lexeme, value);
+  }
 
-    public void set(Token name, Object value) {
-        fields.put(name.lexeme, value);
-    }
-
-    @Override
-    public String toString() {
-        return klass.name + " instance";
-    }
+  @Override
+  public String toString() {
+    return klass.name + " instance";
+  }
 }
